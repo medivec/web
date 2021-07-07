fe69edda8f22b6f30b7e8bed194e3a35a55bd39a
Vicente <vmedina@omitsis.com>
2021-07-07 17:51:03 +0200

    chages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Vicente Medina',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Vicente Medina is a senior developer specialized in WordPress.',
+    siteUrl: 'https://medinarodriguez.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@medivec',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,15 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Vicente Medina</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <p>
-      I'm a Boston-based software engineer who specializes in building (and occasionally designing)
-      exceptional digital experiences. Currently, I'm an engineer at{' '}
-      <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-        Upstatement
+      I'm a Barcelona-based Senior Developer who specializes in WordPress development. Currently, I'm a developer at{' '}
+      <a href="https://www.omitsis.com/" target="_blank" rel="noreferrer">
+        Omitsis
       </a>{' '}
-      focused on building accessible, human-centered products.
+      experts in WordPress, Drupal, Symfony, Magento and Apps.
     </p>
   );
   const five = (
