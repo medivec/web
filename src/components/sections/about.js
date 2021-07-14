1e2e1341912c5155a036cf38c6160804b2fc97a1
Vicente <vmedina@omitsis.com>
2021-07-14 19:13:17 +0200

    more fixes

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'React', 'Eleventy', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['WordPress', 'Docker', 'Sass', 'Gulp', 'Node.js', 'Grunt'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,24 +135,18 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hi! My name is Vicente, I am a Valencian living in Barcelona and I love web development.
+              I started in this scene with Blogger, there I learned to modify the web to adapt it to my needs,
+              along the way I also set up some other forum with phpbb with that
+              I was learning the basics of HTML, CSS &amp; PHP!
             </p>
 
             <p>
-              Fast-forward to today, and I've had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              And I am currently working on developing on WordPress at
+              <a href="https://omitsis.com/">Omitsis</a> for a variety of clients.
             </p>
 
-            <p>Here are a few technologies I've been working with recently:</p>
+            <p>Here are a few technologies/tools I've been working with recently:</p>
           </div>
 
           <ul className="skills-list">
