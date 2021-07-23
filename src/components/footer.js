5a9b45ea19964a8cdde88991bbd3a1abf185fcb9
Vicente <vmedina@omitsis.com>
2021-07-23 17:17:51 +0200

    fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/medivec/v4">
+        <a href="">
           <div>Designed &amp; Built by Brittany Chiang</div>
 
 {/*           {githubInfo.stars && githubInfo.forks && (
